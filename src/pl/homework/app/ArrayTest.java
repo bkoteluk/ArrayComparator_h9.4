e301cb72deb07674d60180b5604e70fe9be84de0
Bogdan Koteluk <bogdan.koteluk@gmail.com>
2019-07-31 09:43:47 +0200

    Homework 9.4

---
+++ src/pl/homework/app/ArrayTest.java
@@ -0,0 +1,16 @@
+package pl.homework.app;
+
+import static pl.homework.lib.ArrayComparator.compare;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] array1 = {0, 1, 3, 3, 4, 5, 6, 7, 8, 9};
+        int[] array2 = {0, 1, 3, 3, 4, 5, 6, 7, 8, 9};
+
+        System.out.println("Tabele są równe ? " + compare(array1, array2));
+
+        int[][] array21 = {{1, 2, 3}, {4, 5}, {} , {0, 10, 11}};
+        int[][] array22 = {{1, 2, 3}, {4, 5}, {7, 8, 9}, {0, 10, 11}};
+        System.out.println("Tabele dwuwymiarowe są równe ? " + compare(array21, array22));
+    }
+}
