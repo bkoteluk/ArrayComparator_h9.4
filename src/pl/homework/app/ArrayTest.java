097efe55f88fdcf1a9f4db046a8039a9924c9200
Bogdan Koteluk <bogdan.koteluk@gmail.com>
2019-07-31 13:41:04 +0200

    Homework 9.4 new methods : isLengthEq and checkArrays were put in class

--- src/pl/homework/app/ArrayTest.java
+++ src/pl/homework/app/ArrayTest.java
@@ -9,7 +9,7 @@ public class ArrayTest {
 
         System.out.println("Tabele są równe ? " + compare(array1, array2));
 
-        int[][] array21 = {{1, 2, 3}, {4, 5}, {} , {0, 10, 11}};
+        int[][] array21 = {{1, 2, 3}, {4, 5}, {7, 8, 9}, {0, 10, 11}};
         int[][] array22 = {{1, 2, 3}, {4, 5}, {7, 8, 9}, {0, 10, 11}};
         System.out.println("Tabele dwuwymiarowe są równe ? " + compare(array21, array22));
     }
--- src/pl/homework/lib/ArrayComparator.java
+++ src/pl/homework/lib/ArrayComparator.java
@@ -2,33 +2,15 @@ package pl.homework.lib;
 
 public class ArrayComparator {
 
-    private static boolean isAnyNull(int[] array1, int[] array2) {
-        return array1 == null || array2 == null;
-    }
-    private static boolean isAnyNull(int[][] array1, int[][] array2) {
-        return array1 == null || array2 == null;
-    }
-
-    private static boolean isAnyZero(int[] array1, int[] array2) {
-        return array1.length == 0 || array2.length == 0;
-    }
-    private static boolean isAnyZero(int[][] array1, int[][] array2) {
-        return array1.length == 0 || array2.length == 0;
-    }
-
     public static boolean compare(int[] array1, int[] array2) {
         int i = 0;
         boolean arrayEqual = true;
-        if (!isAnyNull(array1, array2)) {
-            if (!isAnyZero(array1,array2)) {
-                for(int number : array1) {
-                    if (number != array2[i]) {
-                        arrayEqual =false;
-                    }
-                    i++;
+        if (checkArrays(array1, array2)) {
+            for(int number : array1) {
+                if (number != array2[i]) {
+                    arrayEqual =false;
                 }
-            } else {
-                arrayEqual = false;
+                i++;
             }
         } else {
             arrayEqual = false;
@@ -39,20 +21,47 @@ public class ArrayComparator {
     public static boolean compare(int[][] array1, int[][] array2) {
         int i = 0;
         boolean arrayEqual = true;
-        if (!isAnyNull(array1, array2)) {
-            if (!isAnyZero(array1,array2)) {
-                for (int[] array : array1) {
-                    if (!compare(array,array2[i])) {
+        if (checkArrays(array1, array2)) {
+            for (int[] array : array1) {
+                if (!compare(array,array2[i])) {
                     arrayEqual = false;
                 }
-                 i++;
-                }
-            } else {
-                arrayEqual = false;
+                i++;
             }
         } else {
             arrayEqual = false;
         }
         return arrayEqual;
     }
+
+    public static boolean checkArrays(int[] array1, int[] array2) {
+        return !isAnyNull(array1, array2) && !isAnyZero(array1,array2) && isLengthEq(array1, array2);
+    }
+
+    public static boolean checkArrays(int[][] array1, int[][] array2) {
+        return !isAnyNull(array1, array2) && !isAnyZero(array1,array2) && isLengthEq(array1, array2);
+    }
+
+    private static boolean isAnyNull(int[] array1, int[] array2) {
+        return array1 == null || array2 == null;
+    }
+    private static boolean isAnyNull(int[][] array1, int[][] array2) {
+        return array1 == null || array2 == null;
+    }
+
+    private static boolean isAnyZero(int[] array1, int[] array2) {
+        return array1.length == 0 || array2.length == 0;
+    }
+
+    private static boolean isAnyZero(int[][] array1, int[][] array2) {
+        return array1.length == 0 || array2.length == 0;
+    }
+    private static boolean isLengthEq(int[] array1, int[] array2) {
+        return array1.length == array2.length;
+    }
+
+    private static boolean isLengthEq(int[][] array1, int[][] array2) {
+        return array1.length == array2.length;
+    }
+
 }
